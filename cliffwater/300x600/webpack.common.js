9d9bc3ae0d140205e57052cd174d51de7c37b929
Andrew Dowis <andrew.dowis@fond.co>
2023-11-13 12:08:51 -0800

    unsure

---
+++ cliffwater/300x600/webpack.common.js
@@ -0,0 +1,58 @@
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./deploy/300x600"),
+    filename: "js/bundle.js",
+    libraryTarget: "var",
+    library: "CliffWater",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+
+          // Translates CSS into CommonJS
+          "css-loader",
+
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      // {
+      //   test: /.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+      //   type: "asset/resource",
+      //   generator: {
+      //     filename: "assets/fonts/[name][ext]",
+      //   },
+      // },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./style/main.css",
+    }),
+  ],
+}
